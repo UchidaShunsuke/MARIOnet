04bb503b34876b283cf5d8232d9887c78c719d6e
内田竣介 <134474076+UchidaShunsuke@users.noreply.github.com>
2024-01-04 23:30:39 +0900

    ステージ制作、移動、接地判定

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //インスペクターで設定する
+    public float speed;
+    public GroundCheck ground;
+
+    //プライベート変数
     private Animator anim = null;
+    private Rigidbody2D rb = null;
+    private bool isGround = false;
 
     void Start()
     {
+        //コンポーネントのインスタンスを捕まえる
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        //設置判定を得る
+        isGround = ground.IsGround();
+
+        //キー入力されたら行動する
         float horizontalKey = Input.GetAxis("Horizontal");
+        float xspeed = 0.0f;
 
         if (horizontalKey > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
             anim.SetBool("run", true);
+            xspeed = speed;
         }
         else if (horizontalKey < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             anim.SetBool("run", true);
+            xspeed = -speed;
         }
         else
         {
             anim.SetBool("run", false);
+            xspeed = 0.0f;
         }
+        rb.velocity = new Vector2(xspeed, rb.velocity.y);
     }
 }
